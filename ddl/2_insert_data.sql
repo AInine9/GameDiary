27b31aba0e550f68f396713b484a657ca74c03b4
AInine <ainine.jp@gmail.com>
2022-11-27 05:56:31 +0900

    初期ddlにseed値を追加

---
+++ ddl/2_insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (id, user_name)
+VALUES (1, 'hoge');
+INSERT INTO users (id, user_name)
+VALUES (2, 'huga');
+INSERT INTO users (id, user_name)
+VALUES (3, 'hugu');
+
+INSERT INTO games (name)
+VALUES ('VALORANT');
+INSERT INTO games (name)
+VALUES ('Apex Legends');
+INSERT INTO games (name)
+VALUES ('Overwatch2');
